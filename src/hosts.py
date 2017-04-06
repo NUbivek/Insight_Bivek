9ec2c0fddab72c60a14a931802aaf07829eea4b5
nubivek <bivek@u.northwestern.edu>
2017-04-05 23:50:02 -0400

    Update and rename topIP.py to hosts.py

--- src/hosts.py
+++ src/hosts.py
@@ -9,11 +9,10 @@ def getTop10IPs(filename): #define filename
         hash_map[IPAddress] += 1
       else:
         hash_map[IPAddress] = 1
-
- ####
-  finalRetList=[]
-  for item in hash_map:
-    if (len(finalRetList)<10):
+        
+  finalRetList=[] ##store the final list
+  for item in hash_map: ###initiate a for loop
+    if (len(finalRetList)<10): ##initiate an if else command
       finalRetList.append([item,hash_map[item]])
       finalRetList = sorted(finalRetList, key=operator.itemgetter(1))
     else:
@@ -21,16 +20,15 @@ def getTop10IPs(filename): #define filename
         finalRetList[0]=[item,hash_map[item]] 
         finalRetList = sorted(finalRetList, key=operator.itemgetter(1))
 
-  return sorted(finalRetList, key=operator.itemgetter(1), reverse=True)
+  return sorted(finalRetList, key=operator.itemgetter(1), reverse=True)  ##returns the sorted list
 
-
-def writeFile(tuplesOfHostFreq, filename):
+def writeFile(tuplesOfHostFreq, filename): ##define writefile
   fileHandler = open(filename, 'w')
   for item in tuplesOfHostFreq:
     fileHandler.write("%s, %d\n" % (item[0],item[1]))
   fileHandler.close()
 
-
+##write the output to file
 if __name__ == "__main__":
   topIPAddress = getTop10IPs("log.txt")
   writeFile(topIPAddress, "hosts.txt")
