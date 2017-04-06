136eb008c4961a34c3c8668c196998bcbcb446f8
nubivek <bivek@u.northwestern.edu>
2017-04-05 23:48:30 -0400

    Update resources.py

--- src/resources.py
+++ src/resources.py
@@ -3,15 +3,15 @@
 from heapq import heappush, heappop
 import operator
 
-def getTop10Resources(filename):
+def getTop10Resources(filename): ##define filename
   heap = []
   hashSet = {}
-  with open(filename,'r') as f:
-    for line in f:
-      splitLine = line.split()
+  with open(filename,'r') as f: ### runs when 
+    for line in f: ###Initiate a for loop
+      splitLine = line.split() ###split lines to ensure consistency
       totalBytes = splitLine[-1]
       resource = splitLine[6]
-      if len(resource) <= 1: 
+      if len(resource) <= 1: ### initiate if else to check if the resource is less than or equal to 1
         continue
       else:
         if resource not in hashSet:
@@ -25,7 +25,7 @@ def getTop10Resources(filename):
       # heappush(heap, (-totalBytes, resource))
   sorted_x = sorted(hashSet.items(), key=operator.itemgetter(1))
   sorted_x = sorted_x[::-1]
-  keys = [x[0] for x in sorted_x[:10]]
+  keys = [x[0] for x in sorted_x[:10]] 
   # for x in sorted_x:
   #   print hashSet[x]
   return keys
@@ -37,6 +37,6 @@ def writeFile(tuplesOfHostFreq, filename):
     fileHandler.write(item+'\n')
   fileHandler.close()
 
-if __name__ == "__main__":
+if __name__ == "__main__":  ###write the output to file
   topIPAddress = getTop10Resources("log.txt")
   writeFile(topIPAddress, "resources.txt")
